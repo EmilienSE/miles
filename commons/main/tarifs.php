460b2115d871c7b48378b1d35e8b142559459236
Emilien SECHILARIU <esechilariu@yahoo.fr>
2020-05-13 12:43:26 +0200

    tarifs

--- commons/main/tarifs.php
+++ commons/main/tarifs.php
@@ -11,40 +11,50 @@
     </div>
 
     <div class="row">
-      <div class="col-lg-3">
-       
-      </div>
-      <div class="col-lg-6">
-        <div class="card mb-5 mb-lg-0">
+   	  <div class="col-lg-2"></div>
+      <div class="col-lg-4">
+       <div class="card mb-5 mb-lg-0">
           <div class="card-body" itemscope itemtype="http://schema.org/Offer">
-            <h5 class="card-title text-muted text-uppercase text-center" itemprop="name">Forfait basique</h5>
-            <h6 class="card-price text-center" itemprop="price">500€</h6>
+            <h5 class="card-title text-muted text-uppercase text-center" itemprop="name">½ journée de tournage</h5>
+            <h6 class="card-price text-center" itemprop="price">400€</h6>
             <hr>
             <ul class="fa-ul">
               <li><span class="fa-li"><i class="fa fa-check"></i></span>Prises de vues / Cadrage</li>
+              <li><span class="fa-li"><i class="fa fa-check"></i></span>Prises de son</li>
+              <li><span class="fa-li"><i class="fa fa-check"></i></span>Éclairage</li>
+            </ul>
+            <hr>
+            <h6 class="text-center">Options supplémentaires</h6>
+            <ul class="fa-ul">
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-plus"></i></span>Prises de vues aériennes</li>
+            </ul>
+          </div>
+      </div>
+    </div>
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body" itemscope itemtype="http://schema.org/Offer">
+            <h5 class="card-title text-muted text-uppercase text-center" itemprop="name">½ journée de montage</h5>
+            <h6 class="card-price text-center" itemprop="price">250€</h6>
+            <hr>
+            <ul class="fa-ul">
               <li><span class="fa-li"><i class="fa fa-check"></i></span>Post-Production</li>
-              <li><span class="fa-li"><i class="fa fa-check"></i></span>Restitution de la vidéo</li>
+              <li><span class="fa-li"><i class="fa fa-check"></i></span>Étalonnage</li>
+              <li><span class="fa-li"><i class="fa fa-check"></i></span>Masterisation des sons</li>
             </ul>
             <hr>
             <h6 class="text-center">Options supplémentaires</h6>
             <ul class="fa-ul">
               <li class="text-muted"><span class="fa-li"><i class="fas fa-plus"></i></span>Effets spéciaux en post-production</li>
-              <li class="text-muted"><span class="fa-li"><i class="fas fa-plus"></i></span>Participation à l'écriture du scénario</li>
-              <li class="text-muted"><span class="fa-li"><i class="fas fa-plus"></i></span>Prises de vues aériennes</li>
-              <li class="text-muted"><span class="fa-li"><i class="fas fa-plus"></i></span>Prises de son (discours, ambiance, etc.)</li>
               <li class="text-muted"><span class="fa-li"><i class="fas fa-plus"></i></span>Version longue supplémentaire (format documentaire)</li>
             </ul>
-            <hr>
-            <div class="text-center">
-              <a class="btn scrollto" href="#contact">Demander un devis</a>
-            </div>
           </div>
         </div>
       </div>
-      <!-- Pro Tier -->
-      <div class="col-lg-3">
-        
-      </div>
+   	  <div class="col-lg-2"></div>
+    </div>
+    <div class="text-center">
+        <a class="btn scrollto" href="#contact">Demander un devis</a>
     </div>
 
   </div>
